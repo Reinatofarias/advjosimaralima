45f7b1e82b1c8d9c6cbd95e2a1ac98244d43785f
Dev Team <dev@origo.com.br>
2026-09-21 15:12:57 -0300

    style: suaviza corte da foto no hero

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -46,7 +46,7 @@ export const Hero: React.FC = () => {
                   height={1350}
                   className={styles.photoImage}
                   priority
-                  sizes="(max-width: 992px) 420px, 560px"
+                  sizes="(max-width: 992px) 520px, 680px"
                 />
               </div>
             </div>
